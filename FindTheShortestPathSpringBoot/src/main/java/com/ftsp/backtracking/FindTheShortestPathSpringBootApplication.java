2f4c9e1de07453ac7cf499364191f084e9bd48e6
Ivan Kadiev <i.kadiev16acs@gmail.com>
2020-05-24 21:58:01 +0300

    Initial commit

---
+++ FindTheShortestPathSpringBoot/src/main/java/com/ftsp/backtracking/FindTheShortestPathSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ftsp.backtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTheShortestPathSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTheShortestPathSpringBootApplication.class, args);
+	}
+
+}
