a9bb3ab4e53f337c572d952aca51ebef05647f22
Ivan Kadiev <i.kadiev16acs@gmail.com>
2020-05-24 22:07:48 +0300

    Committing project resources

---
+++ FindTheShortestPathSpringBoot/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO boardparameters (id, boardName, xPixelSize, yPixelSize, xBoxes, yBoxes, initialXPosition, initialYPosition, finalXPosition, finalYPosition, percentChance, wait) VALUES
+  (101, 'largeBoard', 1200, 1200, 20, 20, 1, 1, 18, 18, 0.25, 20),
+  (102, 'mediumBoard', 600, 600, 10, 10, 1, 1, 8, 8, 0.15, 100),
+  (103, 'smallBoard', 300, 300, 5, 5, 1, 1, 3, 3, 0.05, 200);
